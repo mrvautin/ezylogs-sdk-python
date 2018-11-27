7d163e7b61ad45dc522e244e26aafcff8e54cb4c
Mark Moffat <M046724@westpacgroup.com>
2018-11-27 14:47:35 +1030

    Init

---
+++ test.py
@@ -0,0 +1,15 @@
+import ezylogs
+
+ezylogs.setup({
+    "apiKey": "5bf7b64572936a45bce07e3f",
+    "system": "another-system"
+})
+
+ezylogs.debug({"test": "test1 arg1dsdsd"})
+# ezylogs.debug('test1 arg')
+# ezylogs.debug('test1 arg')
+# ezylogs.debug('test1 arg')
+# ezylogs.debug('test1 arg')
+# logging.basicConfig(level=logging.DEBUG)
+
+# logging.debug('test')
